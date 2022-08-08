f196db4296f30f743b6e6980bf2da41c9dbbcd38
markom132 <markomarkoviccb@gmail.com>
2022-08-08 14:00:55 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("We will create an array, please enter arrays lenght");
+        int n = scanner.nextInt();
+        System.out.println("Enter array values");
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Insert value on index " + (i) + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Enter a index of element you want to see");
+        //if you enter index which you don't have in array that will cause (ArrayIndexOutOfBoundsException) unchecked exception
+        int index =  scanner.nextInt();
+
+        //with try-catch we can try our code and if everything is legit try block is executed, if it's not catch block will 'catch' exception and execute that bloch of code :)
+        try {
+            System.out.println("Value of selected index is: " + array[index]);
+
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Entered index don't exists in this array");
+        }
+
+    }
+}
